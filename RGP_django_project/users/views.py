27aa8c9c54746259d80be6c2edbb6e1c05375805
Ralph P <puzonralph@gmail.com>
2020-10-07 22:18:34 -0700

    non-admin login/logout functionality added

--- RGP_django_project/users/views.py
+++ RGP_django_project/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 # ^ django's premade user creation form, we dont need it since we're using UserRegisterForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
 	if request.method == 'POST':
@@ -10,9 +11,13 @@ def register(request):
 		if form.is_valid():
 			form.save() #saves the info and crates the user
 			username = form.cleaned_data.get('username')
-			messages.success(request, f"Account created for {username}!")
-			return redirect('blog-home')
+			messages.success(request, f"Your account has been successfully created!")
+			return redirect('login')
 	else:	
 		form = UserRegisterForm()
 
 	return render(request, 'users/register.html', {'form': form})
+
+@login_required #this is to ensure profile security
+def profile(request):
+	return render(request, 'users/profile.html')
