332c9b0d512d1a45eac2bb8015448de37a01fb8d
Ralph P <puzonralph@gmail.com>
2020-10-13 21:49:29 -0700

    secure create, update delete posts

--- RGP_django_project/RGPblog/urls.py
+++ RGP_django_project/RGPblog/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from . import views # '.' == current directory
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,  PostDeleteView
 
 """
 the first argument from path indicates the url extension that defines
-that view. 
+that view. the "name" for each path is for HTML referencing
 """
 
 
 urlpatterns = [
     #path('admin/', admin.site.urls), the admin mapping
-    path('', views.home, name = 'blog-home'),
-    path('about/', views.about, name = 'blog-about'),
+    path('', PostListView.as_view(), name = 'blog-home'),                         # route to home page
+    # <int:pk> allows us to grab the value from the URL,
+    # detail view expects pk so we use pk:
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),         # route to individual post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),              # route to create post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # route to update post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # route to delete post
+    path('about/', views.about, name = 'blog-about'),                             # route to about page
 ]
--- RGP_django_project/RGPblog/views.py
+++ RGP_django_project/RGPblog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render  #for importing templates
-from django.http import HttpResponse # <- we added this in
 from .models import Post #import models file from same directory
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+	ListView,
+	DetailView,
+	CreateView,
+	UpdateView,
+	DeleteView
+)
 
 # Create your views here. a view == a function below
 
@@ -15,19 +21,75 @@ from .models import Post #import models file from same directory
    * return what we want user to see when they're sent to this route
    * where the logic goes for how we want to handle certain routes"""
 
-#dummy data
 """
 since this is a blog, let's add a few blog 'posts' using static pages
 as a list of dictionaries:
 """
 
+#function based view of home page:
+
 def home(request):
-	#return HttpResponse('<h1>RGP blog Home</h1>') <- original way
 	context = {
-		#'posts': posts <- old way we passed in the dummy data above, now removed.
 		'posts': Post.objects.all() #per the ORM query schema
 	}
 	return render(request, 'RGPblog/home.html', context)
 
+# class based view of home:
+class PostListView(ListView):
+	# this sets the model type inherited to be a "Post" type:
+	model = Post
+
+	# the class defaults to looking for the template in 
+	# <app>/<model>_<viewtype>.html. to change:
+	template_name = "RGPblog/home.html" 
+
+	# our posts are called posts' objects, 
+	# we want the created views to have this property.
+	context_object_name='posts'
+	ordering = ['-date_posted'] #this will order posts from new to old
+
+
 def about(request): #if context is short, pass in function ok
 	return render(request, 'RGPblog/about.html', {'title': "About:"})
+
+class PostDetailView(DetailView):
+	#view the individual blog post
+	model = Post
+	 
+class PostCreateView(LoginRequiredMixin,CreateView): #THE ARG ORDER IS IMPORTANT!
+	#create a new post
+	model = Post
+	fields = ['title', 'content']
+	def form_valid(self, form):
+		#set currently logged in user as author
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): #THE ARG ORDER IS IMPORTANT!
+	#securely update a post
+	model = Post
+	fields = ['title', 'content']
+	def form_valid(self, form):
+		#set currently logged in user as author
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		#if logged in is also author:
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	#delete the blog post
+	model = Post
+	success_url='/'
+
+
+	def test_func(self):
+		post = self.get_object()
+		#if logged in is also author:
+		if self.request.user == post.author:
+			return True
+		return False
